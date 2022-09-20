0539d582d89c6dddee542bccc253cd816b864db6
Mehdi Mortazavian <mehdi.mortazavian@gmail.com>
2022-09-20 04:09:39 -0700

    add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button>{{ name }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: "",
+  },
+};
+</script>
+
+<style scoped>
+button {
+  background: linear-gradient(94.85deg, #246bfd 2.76%, #891bd3 99.01%);
+  border-radius: 8px;
+  border: none;
+  padding: 10px 128px;
+  font-family: sans-serif;
+  color: #f0f0f0;
+}
+</style>

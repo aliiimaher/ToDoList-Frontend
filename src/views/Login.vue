32cba25f5803ef49dc6899db69a36ac29abd7d60
Mehdi Mortazavian <mehdi.mortazavian@gmail.com>
2022-09-23 04:59:56 -0700

    change 1st palceholder in login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Login Page</h1>
-    <TextInput></TextInput>
+    <TextInput placeholder="email"></TextInput>
     <TextInput class="input"></TextInput>
   </div>
 </template>

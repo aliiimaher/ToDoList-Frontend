530f2a10c83d5bb517fe1a590d292dc65c9867b9
Mehdi Mortazavian <mehdi.mortazavian@gmail.com>
2022-09-23 04:56:26 -0700

    1 palceholder changed

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>SignUp Page</h1>
-    <TextInput placeholder="fff" class="input"></TextInput>
+    <TextInput placeholder="name" class="input"></TextInput>
     <TextInput class="input"></TextInput>
     <TextInput class="input"></TextInput>
     <TextInput class="input"></TextInput>

9fb1d7158c1da7cedf310499bb13cf5040c5cba4
Mehdi Mortazavian <mehdi.mortazavian@gmail.com>
2022-09-23 04:25:12 -0700

    add 2 ipnuts to login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,24 @@
 <template>
-    <h1>KIRIIRIIRI</h1>
+  <h1>Login Page</h1>
+  <TextInput></TextInput>
+  <TextInput class="input"></TextInput>
+
 </template>
 
 <script>
+import TextInput from "@/components/TextInput";
+
 export default {
-    
+  components: {TextInput}
 }
 </script>
 
 <style scoped>
+h1{
+  color: #f0f0f0;
+}
 
+.input {
+  margin-top: 20px;
+}
 </style>

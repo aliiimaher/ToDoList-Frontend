636c7d1afac841d3e6205614ffb6b09a66e8f81b
Mehdi Mortazavian <mehdi.mortazavian@gmail.com>
2022-09-24 03:04:45 -0700

    add 2 illustration

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,64 @@
 <template>
-  <div>
-    <router-link to="/login">Login</router-link>
-    <router-link to="/signup">Signup</router-link>
+  <div class="container">
 
-    <!--  <div class="home">-->
-    <!--    <img alt="Vue logo" src="../assets/logo.png"/>-->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div class="card1">
+      <div>
+        <h1>
+          Schedule
+          Your Life
+          <Button name></Button>
+        </h1>
+      </div>
+      <div>
+        <img src="../assets/Schedule.svg" alt="">
+      </div>
+    </div>
+
+    <div class="card2">
+      <div>
+        <img src="../assets/Write.svg" alt="">
+      </div>
+      <div>
+        <h1>Write Your
+          Tasks!
+        </h1>
+      </div>
+    </div>
+
+
+    <div class="card3"></div>
+
+
+    <div class="card4"></div>
+
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-
+import Button from "../components/Button"
 export default {
   name: "HomeView",
-  components: {
-    // HelloWorld,
-  },
+  components: {},
 };
 </script>
 
 <style scoped>
+
+* {
+  color: #f0f0f0;
+}
+
 router-link {
 
+}
+
+.card1 {
+  display: flex;
+}
+
+.card2 {
+  display: flex;
 }
 
 </style>

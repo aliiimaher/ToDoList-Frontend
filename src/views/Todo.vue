851d0d0259c73818f1eee0d6f69c23463680286c
Mehdi Mortazavian <mehdi.mortazavian@gmail.com>
2022-09-25 02:37:06 -0700

    Todo.vue file added to the project

---
+++ src/views/Todo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main-container">
+    <div class="container">
+
+      <div v-for="(item, index) in lists" :key="index">
+        <List :title="lists[index]"/>
+        <br/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo"
+}
+</script>
+
+<style scoped>
+
+</style>

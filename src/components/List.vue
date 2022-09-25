509110d2bcef2aad084c77d802d2dac518d80716
Mehdi Mortazavian <mehdi.mortazavian@gmail.com>
2022-09-25 00:15:56 -0700

    add list view

---
+++ src/components/List.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div>
+      <h5>{{ title }}</h5>
+    </div>
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  props: {
+    title: {},
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Lists.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Lists"
+}
+</script>
+
+<style scoped>
+
+</style>

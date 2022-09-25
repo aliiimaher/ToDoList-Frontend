577eeb4a2588a8caf1f5d0ef51f95a472ea2fc32
Mehdi Mortazavian <mehdi.mortazavian@gmail.com>
2022-09-25 02:38:15 -0700

    SingleTodo.vue file added to the project

---
+++ src/components/SingleTodo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div>
+      <h5>{{ title }}</h5>
+    </div>
+    <div>
+      <img src="../assets/trash.svg" class="icon" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SingleTodo"
+}
+</script>
+
+<style scoped>
+
+</style>

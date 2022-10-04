4f4527fa65fe1a86deb56a3821bb9ef9d0b19970
aliiiimaher <alimohammadmaher@gmail.com>
2022-10-04 22:11:50 +0330

    scale of the btn fixed

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -17,10 +17,12 @@ button {
   background: linear-gradient(94.85deg, #246bfd 2.76%, #891bd3 99.01%);
   border-radius: 8px;
   border: none;
-  padding: 10px 128px;
+  /* padding: 10px 128px; */
   font-family: sans-serif;
+  font-size: 16px;
   color: #f0f0f0;
   /*it is hardcoded and wrong change it as soon as possible*/
-  width: 340px;
+  width: 320px;
+  height: 40px;
 }
 </style>

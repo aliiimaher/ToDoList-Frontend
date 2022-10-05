7ec161362bd70950d0f00ccaf794aa4d46d42d98
aliiiimaher <alimohammadmaher@gmail.com>
2022-10-05 15:12:54 +0330

    whole page added

---
+++ src/views/ForgetPass.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="forget-pass">
+    <div class="title">Forget Your Password? NP :)</div>
+    <div class="main-content">
+        
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>

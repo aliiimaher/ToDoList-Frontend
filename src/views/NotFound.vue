ddfc9bc6fcca0410754c72b84c9069aeb4528f67
aliiiimaher <alimohammadmaher@gmail.com>
2022-10-05 14:47:34 +0330

    new ill added and also have beeen brought to center

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,24 +1,41 @@
 <template>
-    <div class="not-found">
-        <div class="text">
-            <strong>Oops!</strong>
-            Something Went Wrong :(
-        </div>
-        <div class="ill">
-            <img src="../"
-        </div>
+  <div class="not-found">
+    <div class="text">
+      <strong>Oops!</strong>
+      <div>Something Went Wrong :(</div>
     </div>
+    <div class="ill">
+      <img src="../assets/ills/Ill-NotFound.svg" alt="404" class="test" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
+* {
+  margin-left: auto;
+  margin-right: auto;
+}
+
 .text {
-    font-family: sans-serif;
-    font-size: 16px;
+  font-family: sans-serif;
+  font-size: 16px;
+  color: #f0f0f0;
+  text-align: center;
+  padding-top: 120px;
+  padding-bottom: 40px;
+}
+
+.not-found {
+  /* display: flex; */
+  align-items: center;
+  align-content: center;
+}
+
+.test {
+  display: block;
 }
 </style>
